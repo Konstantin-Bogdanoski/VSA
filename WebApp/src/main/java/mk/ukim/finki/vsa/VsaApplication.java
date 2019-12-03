ef1cb2b33304b7685b746b20e6a6189a9ebfc2f4
Konstantin Bogdanoski <konstantin.b@live.com>
2019-12-03 20:46:45 +0100

    - Add SpringBoot App

---
+++ WebApp/src/main/java/mk/ukim/finki/vsa/VsaApplication.java
@@ -0,0 +1,17 @@
+package mk.ukim.finki.vsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Konstantin Bogdanoski (konstantin.b@live.com)
+ */
+
+@SpringBootApplication
+public class VsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsaApplication.class, args);
+    }
+
+}
---
+++ WebApp/src/test/java/mk/ukim/finki/vsa/VsaApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.vsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
